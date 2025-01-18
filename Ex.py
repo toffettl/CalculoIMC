456fe820d5020bf077082e84522a966662446ea1
Felipe Toffetti <139416594+toffettl@users.noreply.github.com>
2025-01-18 16:02:04 -0300

    Exercício feito

---
+++ Ex.py
@@ -0,0 +1,7 @@
+nome = "Felipe Toffetti"
+altura = 1.80
+peso = 95
+imc = peso / (altura * altura)
+print(nome + " tem " + str(altura) + " de altura,")
+print("pesa " + str(peso) + "Kg")
+print("Seu imc é " + str(imc))
